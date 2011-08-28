9ad5216156700989762ba742884b1da031c17919
Raul <raulraat@gmail.com>
2011-08-28 20:35:28 +0300

    add license too

--- multiselect_search.js
+++ multiselect_search.js
@@ -1,3 +1,11 @@
+/**
+ * Multiselect search
+ *
+ * Copyright (c) 2011 Raul Raat
+ * Dual licensed under the MIT and GPL licenses:
+ * http://www.opensource.org/licenses/mit-license.php
+ * http://www.gnu.org/licenses/gpl.html
+ */
 function multiselect_search(ob, settings) {
 	if (!ob || !ob.nodeName || ob.nodeName.toString().toLowerCase() !== 'select' || !ob.multiple) {
 		// do nothing if given element isn't multiple select
